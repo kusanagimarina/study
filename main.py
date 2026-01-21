6fd29889fd1675bc394b03cad8e9dfd8a2f1bda9
marina <3231018@kuis.ac.jp>
2026-01-21 14:37:16 +0900

    day1: hello world

---
+++ main.py
@@ -0,0 +1,12 @@
+name="Marina"
+hours=2
+print ("Hello world")
+print (name)
+print (hours, "hours study today")
+if hours >=2:
+    print ("Good job!")
+else:
+    print ("Keep going!")
+tasks=["install python", "run hello","fix error","success"]
+for task in tasks:
+    print ("DONE:",task)
